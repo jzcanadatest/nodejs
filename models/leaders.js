c49b62153320649ed16b5713bcf24bccd4fc7453
jun zhao <junzhaocanada@gmail.com>
2018-09-25 17:44:59 -0600

    assign2

---
+++ models/leaders.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        default: ''
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+        timestamps: true
+    });
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
