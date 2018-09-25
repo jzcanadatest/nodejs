67402cee5574c18681ef120c7d102599f2f739bf
jun zhao <junzhaocanada@gmail.com>
2018-09-25 16:30:06 -0600

    5baab66624ce85a378d606e5

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +13,16 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+  console.log("Connected correctly to server.");
+}, (err)=>{
+  console.log(err);
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
